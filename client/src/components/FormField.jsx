001b837edf07c5f42c3c6840a46df09609f03af7
Rachel-mac <tanhaosmail@gmail.com>
2024-09-17 19:06:13 -0400

    Implement CreatePost page

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,35 +1,22 @@
 import React from "react";
 
-const FormField = ({
-  labelName,
-  type,
-  name,
-  placeholder,
-  value,
-  handleChange,
-  isSurpriseMe,
-  handleSurpriseMe,
-}) => {
+const FormField = ({labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
     <div>
       <div className="flex items-center gap-2 mb-2">
-        <label
-          htmlFor={name}
-          className="block text-sm font-medium text-gray-900"
-        >
+
+        <label htmlFor={name} className="block text-sm font-medium text-gray-900">
           {labelName}
         </label>
+
         {isSurpriseMe && (
-          <button
-            type="button"
-            onClick={handleSurpriseMe}
-            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
-          >
+          <button type="button" onClick={handleSurpriseMe} className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black">
             Surprise me
           </button>
         )}
       </div>
-      <input
+
+      <input 
         type={type}
         id={name}
         name={name}
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -5,6 +5,7 @@ import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 
+
 const createPost = () => {
   const navigate = useNavigate(); {/* navigate hook: allow us to come back to Home page after create a post */}
   const [form, setForm] = useState({
@@ -15,6 +16,8 @@ const createPost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+
+  {/* generateImage function */}
   const generateImage = async () => {
     if (form.prompt) {
       try {
@@ -37,6 +40,8 @@ const createPost = () => {
     }
   };
 
+
+  {/* handleSubmit function */}
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -49,7 +54,6 @@ const createPost = () => {
           headers: {
             "Content-Type": "application/json",
           },
-
           body: JSON.stringify(form),
         });
 
@@ -65,17 +69,24 @@ const createPost = () => {
     }
   };
 
+
+  {/* handleChange function */}
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+
+  {/* handleSurpriseMe function */}
   const handleSurpriseMe = () => {
     const randomPrompt = getRandomPrompt(form.prompt);
     setForm({ ...form, prompt: randomPrompt });
   };
 
+
   return (
     <section className="max-w-7xl mx-auto">
+
+      {/* Title section */}
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
@@ -83,13 +94,15 @@ const createPost = () => {
           and share them with the community
         </p>
       </div>
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+
+      {/* Form section */}
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}> {/* submit the form when click on the button handleSubmit */}
         <div className="flex flex-col gap-5">
           <FormField
-            labelName="Your name"
+            labelName="Your name" 
             type="text"
             name="name"
-            placeholder="John Doe"
+            placeholder="Rachel Tan"
             value={form.name}
             handleChange={handleChange}
           />
@@ -97,27 +110,21 @@ const createPost = () => {
             labelName="Prompt"
             type="text"
             name="prompt"
-            placeholder="A plush toy robot sitting against a yellow wall"
+            placeholder="Chinggis Khan riding on a huge falcon flying across the sea"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          {/* Container for AI-generated image */}
           <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 h-64 p-3 flex justify-center items-center">
             {form.photo ? (
-              <img
-                src={form.photo}
-                alt={form.prompt}
-                className="w-full h-full object-contain"
-              />
+              <img src={form.photo} alt={form.prompt} className="w-full h-full object-contain"/>
             ) : (
-              <img
-                src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
-              />
+              // If the actual image has not been loaded, use a temporary image
+              <img src={preview} alt="preview" className="w-8/12 h-9/12 object-contain opacity-40"/>
             )}
-
+            {/* If the image is during generating, use Loader component */}
             {generatingImg && (
               <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
                 <Loader />
@@ -125,27 +132,25 @@ const createPost = () => {
             )}
           </div>
         </div>
+
+        {/* Generate image button section */}
         <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className="text-white bg-green-700 font-medium ronded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generatingImg ? "Generating.." : "Generate"}
+          <button type="button" onClick={generateImage} className="text-white bg-green-700 font-medium ronded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"> 
+            {/* Check if it is generating an image right now: if so, display 'Generating' as Button text, otherwise display 'Generate' */}
+            {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
+
+        {/* Share button section */}
         <div className="mt-10">
           <p className="mt-2 text-[#666e75] text-[14px]">
-            Once you have created the image you want, you can share it with
-            others in the community
+            Once you have created the image you want, you can share it with others in the community~
           </p>
-          <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
+          <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
             {loading ? "Loading..." : "Share with the community"}
           </button>
         </div>
+
       </form>
     </section>
   );
